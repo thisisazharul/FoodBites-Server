ab9f67d6b3d4d84c642b1763a20947ec6e2e7479
Md. Azharul Islam <ais14@cse.pstu.ac.bd>
2023-05-04 19:25:17 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const chefRouter = require('./router/chefRouter')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const chefRouter = require("./router/chefRouter");
 
-app.use(cors())
+app.use(cors());
 
 // Using the router;
-app.use('/chefs', chefRouter)
+app.use("/chefs", chefRouter);
 
 // Universal route for server
-app.get('/', (req, res) => {
-    res.send('Chefs Palette server is successfully running...')
-})
+app.get("/", (req, res) => {
+	res.send("Food Bites server is successfully running...");
+});
 
 app.listen(port, () => {
-    console.log(`Chef's Palette successfully running at PORT Number ${port}`)
-})
+	console.log(`Food Bites successfully running at PORT Number ${port}`);
+});
